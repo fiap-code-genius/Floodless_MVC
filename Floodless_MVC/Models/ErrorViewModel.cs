cb64915571f4c720d63c96994d3e0bca73a507da
SunaUezuri <wesley.jane.santos@gmail.com>
2025-05-30 17:57:17 -0300

    Adicionar arquivos de projeto.

---
+++ Floodless_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Floodless_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
